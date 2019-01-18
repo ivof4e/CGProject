38b299b63de7a5b5a09c18251de6fecb046e96c5
ivof4e <ivailo46inov@abv.bg>
2019-01-18 19:02:56 +0200

    4 shapes rectangle square circle ellipse

---
+++ 1701261110/trunk/CGProject/src/Processors/DialogProcessor.cs
@@ -0,0 +1,151 @@
+﻿using Draw.src.Model;
+using System;
+using System.Drawing;
+
+namespace Draw
+{
+	/// <summary>
+	/// Класът, който ще бъде използван при управляване на диалога.
+	/// </summary>
+	public class DialogProcessor : DisplayProcessor
+	{
+		#region Constructor
+		
+		public DialogProcessor()
+		{
+		}
+		
+		#endregion
+		
+		#region Properties
+		
+		/// <summary>
+		/// Избран елемент.
+		/// </summary>
+		private Shape selection;
+		public Shape Selection {
+			get { return selection; }
+			set { selection = value; }
+		}
+		
+		/// <summary>
+		/// Дали в момента диалога е в състояние на "влачене" на избрания елемент.
+		/// </summary>
+		private bool isDragging;
+		public bool IsDragging {
+			get { return isDragging; }
+			set { isDragging = value; }
+		}
+		
+		/// <summary>
+		/// Последна позиция на мишката при "влачене".
+		/// Използва се за определяне на вектора на транслация.
+		/// </summary>
+		private PointF lastLocation;
+		public PointF LastLocation {
+			get { return lastLocation; }
+			set { lastLocation = value; }
+		}
+		
+		#endregion
+		
+		/// <summary>
+		/// Проверява дали дадена точка е в елемента.
+		/// Обхожда в ред обратен на визуализацията с цел намиране на
+		/// "най-горния" елемент т.е. този който виждаме под мишката.
+		/// </summary>
+		/// <param name="point">Указана точка</param>
+		/// <returns>Елемента на изображението, на който принадлежи дадената точка.</returns>
+		public Shape ContainsPoint(PointF point)
+		{
+			for(int i = ShapeList.Count - 1; i >= 0; i--){
+				if (ShapeList[i].Contains(point)){
+					ShapeList[i].FillColor = Color.Red;
+						
+					return ShapeList[i];
+				}	
+			}
+			return null;
+		}
+		
+		/// <summary>
+		/// Транслация на избраният елемент на вектор определен от <paramref name="p>p</paramref>
+		/// </summary>
+		/// <param name="p">Вектор на транслация.</param>
+		public void TranslateTo(PointF p)
+		{
+			if (selection != null) {
+				selection.Location = new PointF(selection.Location.X + p.X - lastLocation.X, selection.Location.Y + p.Y - lastLocation.Y);
+				lastLocation = p;
+			}
+		}
+        /// <summary>
+		/// Добавя примитив - правоъгълник на произволно място върху клиентската област.
+		/// </summary>
+		public void AddRandomRectangle()
+        {
+            Random rnd = new Random();
+            int x = rnd.Next(100, 1000);
+            int y = rnd.Next(100, 600);
+
+            RectangleShape rect = new RectangleShape(new Rectangle(x, y, 100, 200));
+            rect.FillColor = Color.White;
+            rect.BorderColor = Color.Black;
+
+            ShapeList.Add(rect);
+        }
+        // Добавяне на рандом Квадрат
+        public void AddRandomSquare()
+        {
+            Random rnd = new Random();
+            int x = rnd.Next(100, 1000);
+            int y = rnd.Next(100, 600);
+
+            SquareShape square = new SquareShape(new Rectangle(x, y, 100, 100));
+            square.FillColor = Color.White;
+            square.BorderColor = Color.Black;
+
+            ShapeList.Add(square);
+        }
+
+        // Добавяне на рандом Елипса
+        public void AddRandomEllipse()
+        {
+            Random rnd = new Random();
+            int x = rnd.Next(100, 1000);
+            int y = rnd.Next(100, 600);
+
+            EllipseShape ellipse = new EllipseShape(new Rectangle(x, y, 200, 100));
+            ellipse.FillColor = Color.White;
+            ellipse.BorderColor = Color.Black;
+
+            ShapeList.Add(ellipse);
+        }
+
+        // Добавяне на рандом Кръг
+        public void AddRandomCircle()
+        {
+            Random rnd = new Random();
+            int x = rnd.Next(100, 1000);
+            int y = rnd.Next(100, 600);
+
+            CircleShape circle = new CircleShape(new Rectangle(x, y, 100, 100));
+            circle.FillColor = Color.White;
+            circle.BorderColor = Color.Black;
+
+            ShapeList.Add(circle);
+        }
+
+
+        public void SetFillColor(Color color)
+        {
+            if (Selection != null)
+            {
+                Selection.FillColor = color;
+            }
+        }
+
+
+
+    }
+}
